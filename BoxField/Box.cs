ce3a7784d9ac9505dc2b9bd972af60fa756b8f9c
cierdieb649 <cierdieb649@SCS-D-360QHN>
2019-02-20 10:04:47 -0500

    End of Class February 20th 2019

---
+++ BoxField/Box.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxField
+{
+    class Box
+    {
+        public int x, y, size, colour;
+        //Need Colour Eventually
+        
+        public Box (int _x, int _y, int _size, int _colour)
+        {
+            x = _x;
+            y = _y;
+            size = _size;
+            if (_colour == 1)
+            {
+
+            }
+        }
+    }
+}
--- BoxField/Screens/GameScreen.cs
+++ BoxField/Screens/GameScreen.cs
@@ -17,13 +17,18 @@ namespace BoxField
 
         //used to draw boxes on screen
         SolidBrush boxBrush = new SolidBrush(Color.White);
-        
-        //TODO - create a list to hold a column of boxes        
 
+        //lists to hold columns of boxes        
+        new List<Box> leftBoxes = new List<Box>();
+        new List<Box> rightBoxes = new List<Box>();
 
+        //Box Timer
+        int boxTimer;
+        public int location;
         public GameScreen()
         {
             InitializeComponent();
+            OnStart();
         }
 
         /// <summary>
@@ -31,7 +36,12 @@ namespace BoxField
         /// </summary>
         public void OnStart()
         {
+            location = 25;
             //TODO - set game start values
+            Box b1 = new Box(location, 0, 25);
+            leftBoxes.Add(b1);
+            Box b2 = new Box(this.Width - location - b1.size, 0, 25);
+            rightBoxes.Add(b2);
         }
 
         private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
@@ -65,17 +75,49 @@ namespace BoxField
         private void gameLoop_Tick(object sender, EventArgs e)
         {
             //TODO - update location of all boxes (drop down screen)
+            foreach (Box b in leftBoxes)
+            {
+                b.y = b.y + 5;
+            }
 
+            foreach (Box b in rightBoxes)
+            {
+                b.y = b.y + 5;
+            }
             //TODO - remove box if it has gone of screen
-
+            if (leftBoxes[0].y >= this.Height)
+            {
+                leftBoxes.Remove(leftBoxes[0]);
+            }
+            if (rightBoxes[0].y >= this.Height)
+            {
+                rightBoxes.Remove(rightBoxes[0]);
+            }
             //TODO - add new box if it is time
-
+            boxTimer++;
+            if (boxTimer % 8 == 0)
+            {
+                Box b1 = new Box(location, 0, 25);
+                leftBoxes.Add(b1);
+                Box b2 = new Box(this.Width - location - b1.size, 0, 25);
+                rightBoxes.Add(b2);
+                boxTimer = 0;
+            }
             Refresh();
         }
 
         private void GameScreen_Paint(object sender, PaintEventArgs e)
         {
             //TODO - draw boxes to screen
+            foreach (Box b in leftBoxes)
+            {
+                e.Graphics.FillRectangle(boxBrush, b.x, b.y, b.size, b.size);
+            }
+            foreach (Box b in rightBoxes)
+            {
+                e.Graphics.FillRectangle(boxBrush, b.x, b.y, b.size, b.size);
+            }
+
         }
     }
 }
